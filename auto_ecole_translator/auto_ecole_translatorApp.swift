6d178bc51ff9ee6f594f8ac0e49d99c1e305fc64
aminataguia <amina.taguia.dev.ia@gmail.com>
2025-01-29 12:30:13 +0100

    Initial Commit

---
+++ auto_ecole_translator/auto_ecole_translatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  auto_ecole_translatorApp.swift
+//  auto_ecole_translator
+//
+//  Created by taguia amina on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct auto_ecole_translatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ auto_ecole_translatorTests/auto_ecole_translatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  auto_ecole_translatorTests.swift
+//  auto_ecole_translatorTests
+//
+//  Created by taguia amina on 29/01/2025.
+//
+
+import Testing
+
+struct auto_ecole_translatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
